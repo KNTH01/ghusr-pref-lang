bbb6af5255d662be534d118f23143585138e2137
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-01-17 13:43:44 +0100

    Compute data to get language repartitio

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 require('dotenv').config()
 const { GraphQLClient, gql } = require('graphql-request')
 
+function computeData(data) {
+  const edgeRepositories = data.repositoryOwner.repositories.edges
+  const result = {}
+
+  for (let nodeRepository of edgeRepositories) {
+    nodeRepository.node.languages.edges.map((item) => {
+      const size = item.size
+      const name = item.node.name
+
+      result[name] = result[name] ? result[name] + size : size
+    })
+  }
+
+  return Object.entries(result)
+    .sort(([, a], [, b]) => b - a)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {})
+}
+
 async function main() {
   const endpoint = 'https://api.github.com/graphql'
   const graphQLClient = new GraphQLClient(endpoint, {
@@ -9,9 +27,10 @@ async function main() {
     },
   })
 
+  const username = 'KevinNTH'
   const query = gql`
-    {
-      repositoryOwner(login: "KevinNTH") {
+    query getLanguagesRepartition($username: String!) {
+      repositoryOwner(login: $username) {
         repositories(last: 100, privacy: PUBLIC) {
           totalCount
           edges {
@@ -33,9 +52,11 @@ async function main() {
       }
     }
   `
-  const data = await graphQLClient.request(query)
+  const data = await graphQLClient.request(query, { username })
+  const result = computeData(data)
 
-  console.log(JSON.stringify(data, undefined, 2))
+  console.log(`Language repartition (in Bytes) for ${username}`)
+  console.log(result)
 }
 
 main().catch((error) => console.error(error))
